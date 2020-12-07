1107329ed0cb14e4ce84b3e6e2638ea95594806a
creator-sona <>
2020-12-07 23:29:31 +0900

    css style update

--- src/components/ListPage.js
+++ src/components/ListPage.js
@@ -7,11 +7,13 @@ function ListPage() {
     });
 
     return (
-        <div className="List_page">
+        <div className="list_page">
             <header>리스트{pageInfo.mode}</header>
-            <input type="button" onClick={() => setPageInfo({mode : 'write'})}/>
-            <TopMenu pageInfo={pageInfo}/>
-            <List />
+            <main>
+                <input type="button" value="click" onClick={() => setPageInfo({mode : 'write'})}/>
+                <TopMenu pageInfo={pageInfo}/>
+                <List />
+            </main>
         </div>
     );
 }
@@ -19,34 +21,56 @@ function ListPage() {
 function TopMenu({pageInfo}){
     console.log(pageInfo);
     return (
-        <div className="top_menu">
-            <div className="search_box">
-                <input type="text"/>
-                <button>검색</button>
+        <section className="top_menu">
+            <div className="wrap">
+                <div className="search_box">
+                    <input type="text" placeholder="라이언"/>
+                    <input className="btn_sch" type="button" value="검색"/>
+                </div>
+                <input className="btn_pop" type="button" value="조건"/>
+                <input className="btn_add" type="button" value="등록" />
             </div>
-            <input className="btn_pop" type="button" value="조건검색"/>
-            <input className="btn_add" type="button" value="등록" />
-        </div>
+        </section>
     );
 }
 
 function List(){
     return (
-        <div>
-            <p className="year">---------- 2020 ----------</p>
+        <section>
+            <p className="year_line"><span>2020</span></p>
             <ul className="list">
-                <li>
+                <li className="give_ico">
                     <div className="left">
                         <span>12.12</span>
-                        <span>라이언 결혼 <br/> 강남 카카오웨딩홀</span>
+                        <span className="desc">라이언 결혼 <br/> <small>강남 카카오웨딩홀</small></span>
                     </div>
                     <div className="right">
                         <span>현금</span>
-                        <span>100,000</span>
+                        <span className="price">100,000</span>
+                    </div>
+                </li>
+                <li className="take_ico">
+                    <div className="left">
+                        <span>12.12</span>
+                        <span className="desc">어피치 생일 <br/> <small></small></span>
+                    </div>
+                    <div className="right">
+                        <span>기프티콘</span>
+                        <span className="price">15,000</span>
+                    </div>
+                </li>
+                <li className="give_ico">
+                    <div className="left">
+                        <span>12.12</span>
+                        <span className="desc">튜브김튜뷰 돌잔치 <br/> <small>튜브김튜뷰유튜뷰어쩌고저쩌고</small></span>
+                    </div>
+                    <div className="right">
+                        <span>현금</span>
+                        <span className="price">50,000</span>
                     </div>
                 </li>
             </ul>
-        </div>
+        </section>
     );
 }
 
